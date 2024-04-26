9a08a69b2a4275c074ba5948eff22c0b3b921738
Ruslan Voshchylo <ruslan.voshchylo2@gmail.com>
2024-04-26 18:12:47 +0300

    add remove markers feature

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import GoogleMap from 'react-map-gl';
+import React, { useEffect, useRef, useState } from 'react';
+import GoogleMap, { MapRef } from 'react-map-gl'; //MarkerDragEvent, ViewStateChangeEvent
 import { Layer, Source } from 'react-map-gl';
 import { MarkerInterface } from '../../types/Marker';
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl'; // { LngLat, MapLayerMouseEvent }
 import { clusterLayer, clusterCountLayer, unclusteredPointLayer, unclusteredPointTextLayer } from './layers';
 import { FeatureCollection, GeoJsonProperties, Geometry } from 'geojson';
 import styles from './Map.module.css';
@@ -12,9 +12,12 @@ import { updateMarkers } from '../../utils/updateMarkers';
 
 const Map: React.FC = () => {
   const [markers, setMarkers] = useState<MarkerInterface[]>([]);
-  const [isLoaded, setIsLoaded] = useState(false); 
-  // const [, logEvents] = useState<Record<string, LngLat>>({});
-  console.log(markers);
+  const [isLoaded, setIsLoaded] = useState(false);
+  // const [currentMarker, setCurrentMarker] = useState<MarkerInterface | null>(null);
+  // const [dragPan, setDragPan] = useState(true);
+  // const [dragRotate, setDragRotate] = useState(true);
+
+  const mapRef = useRef<MapRef>(null);
 
   useEffect(() => {
     getMarkers()
@@ -53,35 +56,65 @@ const Map: React.FC = () => {
     }))
   };
 
-  // const handleRightClick = (event: React.MouseEvent) => {
-  //   event.preventDefault();
-  //   const markersCopy = [...markers];
-  //   const index = Number(event.currentTarget.textContent) - 1;
-  //   markersCopy.splice(index, 1);
-  //   setMarkers(markersCopy);
-  // };
+  const handleRightClick = (event: mapboxgl.MapLayerMouseEvent) => {
+    event.preventDefault();
+    const markersCopy = [...markers];
+    const features = mapRef.current?.queryRenderedFeatures(event.point)[0];
+    if (!features || !features.properties) {
+      return;
+    }
+    const markerId = features.properties.markerId;
+    const index = markersCopy.findIndex(marker => marker.id === markerId);
+    markersCopy.splice(index, 1);
+    setMarkers(markersCopy);
+  };
 
-  // const moveMarker = (event: MarkerDragEvent, markerId: number) => {
+  // const moveMarker = (event: ViewStateChangeEvent, markerId: number) => {
   //   const markersCopy = [...markers];
+
   //   const markerIndex = markersCopy.findIndex(marker => marker.id === markerId);
 
-  //   markersCopy[markerIndex] = { id: markerId, lat: event.lngLat.lat, lng: event.lngLat.lng };
+  //   markersCopy[markerIndex] = { id: markerId, lat: event.viewState.latitude, lng: event.viewState.longitude };
   //   setMarkers(markersCopy);
+
   // };
 
-  // const onMarkerDrag = useCallback((event: MarkerDragEvent, markerId: number) => {
-  //   logEvents(_events => ({ ..._events, onDrag: new LngLat(event.lngLat.lng, event.lngLat.lat) }));
+  // const onDragStart = useCallback((event: ViewStateChangeEvent) => {
+  //   logEvents(_events => ({ ..._events, onDragStart: new LngLat(event.viewState.longitude, event.viewState.latitude) }));
+  //   const features = mapRef.current?.queryRenderedFeatures(event.point)[0];
+  //   if (!features || !features.properties) {
+  //     return;
+  //   }
+  //   setCurrentMarker(markers.find(marker => marker.id === features.properties.markerId) || null);
+  //   setDragPan(false);
+  //   setDragRotate(false);
+  // }, [markers]);
+
+  // const onMarkerDrag = useCallback((event: ViewStateChangeEvent) => {
+  //   logEvents(_events => ({ ..._events, onDrag: new LngLat(event.viewState.longitude, event.viewState.latitude) }));
+  //   if (!currentMarker) {
+  //     return;
+  //   }
+  //   const markerId = currentMarker.id;
   //   moveMarker(event, markerId);
   // }, [markers]);
 
-  // const onMarkerDragEnd = useCallback((event: MarkerDragEvent, markerId: number) => {
-  //   logEvents(_events => ({ ..._events, onDragEnd: new LngLat(event.lngLat.lng, event.lngLat.lat) }));
+  // const onMarkerDragEnd = useCallback((event: ViewStateChangeEvent) => {
+  //   logEvents(_events => ({ ..._events, onDragEnd: new LngLat(event.viewState.longitude, event.viewState.latitude) }));
+  //   if (!currentMarker) {
+  //     return;
+  //   }
+  //   const markerId = currentMarker.id;
   //   moveMarker(event, markerId);
+  //   setCurrentMarker(null);
+  //   setDragPan(true);
+  //   setDragRotate(true);;
   // }, [markers]);
 
   return (
     <div className={styles.container}>
       <GoogleMap
+        ref={mapRef}
         mapboxAccessToken="pk.eyJ1IjoicnVzZnVzcm8iLCJhIjoiY2x2Z2NucW5yMHZ6bTJsbDk1MWF1d2t3eiJ9.yUhQ0LxrozmR53GVz7UUbw"
         initialViewState={{
           latitude: 49.843644,
@@ -90,8 +123,13 @@ const Map: React.FC = () => {
         }}
         style={{ width: 1000, height: 800 }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-        onClick={(e: mapboxgl.MapLayerMouseEvent) => handleMapClick({ lat: e.lngLat.lat, lng: e.lngLat.lng })}
-
+        onClick={(e: mapboxgl.MapLayerMouseEvent) => {
+          handleMapClick({ lat: e.lngLat.lat, lng: e.lngLat.lng })
+        }}
+        onContextMenu={(e: mapboxgl.MapLayerMouseEvent) => handleRightClick(e)}
+        // onDrag={(e: ViewStateChangeEvent) => onMarkerDrag(e)}
+        // onDragStart={(e: ViewStateChangeEvent) => onDragStart(e)}
+        // onDragEnd={(e: ViewStateChangeEvent) => onMarkerDragEnd(e)}
       >
         <Source
           id="earthquakes"
@@ -101,10 +139,10 @@ const Map: React.FC = () => {
           clusterMaxZoom={14}
           clusterRadius={50}
         >
-          <Layer {...clusterLayer} />
-          <Layer {...clusterCountLayer} />
-          <Layer {...unclusteredPointLayer} />
-          <Layer {...unclusteredPointTextLayer} />
+          <Layer {...clusterLayer} key={1} />
+          <Layer {...clusterCountLayer} key={2} />
+          <Layer {...unclusteredPointLayer} key={3} />
+          <Layer {...unclusteredPointTextLayer} key={4} />
         </Source>
       </GoogleMap>
     </div>
